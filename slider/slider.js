b38f71af63e3989ebd68012a85ca2870d8b79609
VadmanBat <VabmanBat@gmail.com>
2023-04-12 00:12:04 +0300

    final-slider

--- slider/slider.js
+++ slider/slider.js
@@ -1,24 +1,28 @@
-var n_img = 3;
+var n_img = 5;
 var corrente = 1;
 
 function succ()
 {
     document.querySelector(".left-banner__slider__item:nth-child(" + corrente + ")").style.display = "none";
+    document.querySelector(".left-banner__control-list__item:nth-child(" + corrente + ")").style.background = "#9B9B9B";
 
     if (++corrente > n_img)
         corrente = 1;
 
     document.querySelector(".left-banner__slider__item:nth-child(" + corrente + ")").style.display = "block";
+    document.querySelector(".left-banner__control-list__item:nth-child(" + corrente + ")").style.background = "white";
 }
 
 function prec()
 {
     document.querySelector(".left-banner__slider__item:nth-child(" + corrente + ")").style.display = "none";
+    document.querySelector(".left-banner__control-list__item:nth-child(" + corrente + ")").style.background = "#9B9B9B";
 
     if (--corrente == 0)
         corrente = n_img;
 
     document.querySelector(".left-banner__slider__item:nth-child(" + corrente + ")").style.display = "block";
+    document.querySelector(".left-banner__control-list__item:nth-child(" + corrente + ")").style.background = "white";
 }
 
-setInterval(succ, 5000);
+//setInterval(succ, 5000);
