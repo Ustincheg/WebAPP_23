83c8630f9ef7ca7e0f0d043e5b7a7df8ae9bdb91
sowaxa <andryblackxaxa@mail.ru>
2023-05-12 09:21:42 +0300

    PK2

---
+++ PK2/1.js
@@ -0,0 +1,54 @@
+// g=Math.floor(Math.random()*(11-1)+1)
+// console.log("введите число от 1 до 10")
+// a=prompt("введите число от 1 до 10")
+// if (a==g) (alert("Хорошая работа")); 
+// else alert("Не соответсвует");
+
+// g = 0;
+// a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i] % 2 == 0)
+//         g = g + a[i];
+// }
+// alert(g)
+
+// a=parseInt(prompt("введите первое число"))
+// let b=parseInt(prompt("введите второе число"))
+// c=prompt("введите + или -")
+// if (c=="+")alert(a+b);
+// else if(c=="-") alert(a-b);
+// else alert("ошибка!")
+
+// a = [5, 2, 3, 1, 5, 6, 7, 6, 9, 3];
+// for (i = 1; i < a.length; 0)
+//     if (a[i] < a[i - 1]) {
+//         g = a[i]
+//         a[i] = a[i - 1]
+//         a[i - 1] = g
+//         i = i - 1
+//     }
+//     else i++
+// alert(a)
+
+// arr1 = [5, 2, 3, 1, 5, 6, 7, 6, 9, 3];
+// arr2 = arr1.filter((item,i) => arr1.indexOf(item) == i);
+// alert(arr2)
+
+// a=prompt("введите значение")
+// b=isFinite(a)
+// if(b==1)
+// alert ("True")
+// else alert("Else")
+
+// a = prompt("введите строку")
+// a=a.split('')
+// for (i = 1; i < a.length; 0)
+//     if (a[i] < a[i - 1]) {
+//         g = a[i]
+//         a[i] = a[i - 1]
+//         a[i - 1] = g
+//         i = i - 1
+//     }
+//     else i++
+// a=a.join('')
+// alert(a)
